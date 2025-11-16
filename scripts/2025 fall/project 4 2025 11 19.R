7d5a9a29e6ead626ae1a981158dc9fa94c5e7b33
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2025-11-16 09:54:20 -0500

    Update project 4 2025 11 19.R

--- scripts/2025 fall/project 4 2025 11 19.R
+++ scripts/2025 fall/project 4 2025 11 19.R
@@ -38,12 +38,19 @@ city.filter <- c("Austin TX",
 
 # DRUG USAGE DATA
 ### read in drug usage CSV
-drug.usage <- read.csv("C:/Users/danna/Downloads/drugusage.csv", stringsAsFactors = FALSE)
+drug.usage <- read.csv("https://raw.githubusercontent.com/matthew-danna/cities-and-crime/refs/heads/main/data/american_addicition_centers.csv", 
+                       stringsAsFactors = FALSE)
 drug.usage <- drug.usage[c(1:5,8:15)]
 drug.usage$total <- drug.usage$Marijuana + drug.usage$Cocaine +
   drug.usage$Heroin + drug.usage$Meth
 
 drug.usage.similar <- drug.usage %>% filter(State %in% city.filter)
+
+names(drug.usage.similar) <- c("city.state",  "use.marijuana", "use.cocaine", "use.heroin", "use.meth", 
+                               "use.diff.marijuana", "use.diff.cocaine", "use.diff.heroin", "use.diff.meth", 
+                               "use.rank.marijuana", "use.rank.cocaine", "use.rank.heroin", "use.rank.meth", "use.total")
+
+drug.usage.similar <- drug.usage.similar[c(1,14,2,6,10,3,7,11,4,8,12,5,9,13)]
 
 # DRUG OVERDOSE
 drug.overdose <- read.csv("https://raw.githubusercontent.com/matthew-danna/cities-and-crime/refs/heads/main/data/final.overdoses.csv",
@@ -64,5 +71,107 @@ drug.overdose.category <- drug.overdose %>%
     values_from = overdoses,
     values_fill = list(overdoses = 0)
   )
+
+city.county <- tribble(
+  ~city,                 ~county.code,
+  "Austin TX",           48453,   # Travis County  [oai_citation:0‡U.S. Office of Personnel Management](https://www.opm.gov/policy-data-oversight/pay-leave/salaries-wages/2024/locality-pay-area-definitions/?utm_source=chatgpt.com)  
+  "Baltimore MD",        24510,   # Baltimore City (independent)  [oai_citation:1‡HMDB](https://www.hmdb.org/countyoverlay/countyseatlist.asp?utm_source=chatgpt.com)  
+  "Boston MA",           25025,   # Suffolk County  
+  "Charlotte NC",        37119,   # Mecklenburg County  
+  "Chicago IL",          17031,   # Cook County  [oai_citation:2‡University Library](https://www.library.illinois.edu/max/collections/air-photos-new/air-photo-indexes/fips_codes_il_counties/?utm_source=chatgpt.com)  
+  "Cleveland OH",        39035,   # Cuyahoga County  
+  "Columbus OH",         39049,   # Franklin County  
+  "Dallas TX",           48113,   # Dallas County (major)  
+  "Denver CO",           08031,   # Denver County  
+  "Detroit MI",          26163,   # Wayne County  
+  "El Paso TX",          48141,   # El Paso County  
+  "Houston TX",          48201,   # Harris County  [oai_citation:3‡Wikipedia](https://en.wikipedia.org/wiki/Houston?utm_source=chatgpt.com)  
+  "Houston TX",          48157,   # Fort Bend County (part)  [oai_citation:4‡U.S. Office of Personnel Management](https://www.opm.gov/policy-data-oversight/pay-leave/salaries-wages/2011/locality-pay-area-definitions/?utm_source=chatgpt.com)  
+  "Houston TX",          48339,   # Montgomery County (part)  [oai_citation:5‡U.S. Office of Personnel Management](https://www.opm.gov/policy-data-oversight/pay-leave/salaries-wages/2011/locality-pay-area-definitions/?utm_source=chatgpt.com)  
+  "Indianapolis IN",     18097,   # Marion County  
+  "Kansas City MO",      29095,   # Jackson County  [oai_citation:6‡Wikipedia](https://en.wikipedia.org/wiki/List_of_counties_in_Missouri?utm_source=chatgpt.com)  
+  "Kansas City MO",      29047,   # Clay County  [oai_citation:7‡mcdc.missouri.edu](https://mcdc.missouri.edu/geography/reference/mocnty2uauc.html?utm_source=chatgpt.com)  
+  "Kansas City MO",      29037,   # Cass County  [oai_citation:8‡mcdc.missouri.edu](https://mcdc.missouri.edu/geography/reference/mocnty2uauc.html?utm_source=chatgpt.com)  
+  "Kansas City MO",      29165,   # Platte County  [oai_citation:9‡Wikipedia](https://en.wikipedia.org/wiki/Platte_County%2C_Missouri?utm_source=chatgpt.com)  
+  "Las Vegas NV",        32003,   # Clark County  
+  "Long Beach CA",       06037,   # Los Angeles County  
+  "Los Angeles CA",      06037,   # Los Angeles County  
+  "Louisville KY",       21111,   # Jefferson County  
+  "Memphis TN",          47157,   # Shelby County  
+  "Milwaukee WI",        55079,   # Milwaukee County  
+  "Minneapolis MN",      27053,   # Hennepin County  
+  "Nashville TN",        47037,   # Davidson County  
+  "New York City NY",    36005,   # Bronx  
+  "New York City NY",    36047,   # Kings (Brooklyn)  
+  "New York City NY",    36061,   # New York (Manhattan)  
+  "New York City NY",    36081,   # Queens  
+  "New York City NY",    36085,   # Richmond (Staten Island)  
+  "Oakland CA",          06001,   # Alameda County  
+  "Oklahoma City OK",    40109,   # Oklahoma County  
+  "Oklahoma City OK",    40017,   # Canadian County  
+  "Oklahoma City OK",    40027,   # Cleveland County  
+  "Oklahoma City OK",    40125,   # Pottawatomie County  
+  "Philadelphia PA",     42101,   # Philadelphia County  
+  "Phoenix AZ",          04013,   # Maricopa County  
+  "Phoenix AZ",          04021,   # (Note: some parts may extend into Pinal County)  
+  "Portland OR",         41051,   # Multnomah County  
+  "San Antonio TX",      48029,   # Bexar County  
+  "San Diego CA",        06073,   # San Diego County  
+  "San Francisco CA",    06075,   # San Francisco County (city-county)  [oai_citation:10‡HMDB](https://www.hmdb.org/countyoverlay/countyseatlist.asp?utm_source=chatgpt.com)  
+  "San Jose CA",         06085,   # Santa Clara County  
+  "Seattle WA",          53033,   # King County  
+  "Tucson AZ",           04019,   # Pima County  
+  "Washington DC",       11001    # District of Columbia (equivalent to county)  [oai_citation:11‡HMDB](https://www.hmdb.org/countyoverlay/countyseatlist.asp?utm_source=chatgpt.com)  
+)
+
+drug.od <- drug.overdose.category %>%
+  right_join(city.county.lookup)
+
+names(drug.od) <- c("county", "county.code", 
+                    "tobacco", "undetermined1", "psychoactive1", "antiepileptic1", "hallucinogens1", "undetermined2",
+                    "multiple1", "hallucinogens2", "opioids1", "opioids2", "sedatives1", "cocaine1", "stimulants1", 
+                    "stimulants2", "solvents", "antiepileptic2", "antiepileptic3", "hallucinogens3", "multiple2", 
+                    "cannabinoids1", "cocaine2", "multiple3", "assault" ,"psychoactive2", "opioids3", "cocaine3", 
+                    "analgesics1", "cannabinoids2", "sedatives2", "analgesics2", "pyschoactive3", "stimulants3", 
+                    "opioids4", "analgesics3",
+                    "city")
+
+drug.od$undetermined <- drug.od$undetermined1 + drug.od$undetermined2
+drug.od$psychoactive <- drug.od$psychoactive1 + drug.od$psychoactive2 + drug.od$pyschoactive3
+drug.od$antiepileptic <- drug.od$antiepileptic1 + drug.od$antiepileptic2 + drug.od$antiepileptic3
+drug.od$hallucinogens <- drug.od$hallucinogens1 + drug.od$hallucinogens2 + drug.od$hallucinogens3
+drug.od$multiple <- drug.od$multiple1 + drug.od$multiple2 + drug.od$multiple3
+drug.od$opioids <- drug.od$opioids1 + drug.od$multiple2 + drug.od$multiple3
+drug.od$sedatives <- drug.od$sedatives1 + drug.od$sedatives2
+drug.od$cocaine <- drug.od$cocaine1 + drug.od$cocaine2 + drug.od$cocaine3
+drug.od$stimulants <- drug.od$stimulants1 + drug.od$stimulants2 + drug.od$stimulants3
+drug.od$cannabinoids <- drug.od$cannabinoids1 + drug.od$cannabinoids2
+drug.od$analgesics <- drug.od$analgesics1 + drug.od$analgesics2 + drug.od$analgesics3
+
+overdoses <- drug.od[c(37,1,3,17,25,38:48)]
+
+names(overdoses) <- c("city.state", "county", "od.tobacco", "od.solvents", "od.assault", "od.undetermined", 
+                      "od.psychoactive", "od.antiepileptic", "od.hallucinogens", "od.multiple", "od.opioids",
+                      "od.sedatives", "od.cocaine", "od.stimulants", "od.cannabinoids", "od.analgesics")
+
+# Collapse all county rows into one row per city
+city.overdoses <- overdoses %>%
+  group_by(city.state) %>%
+  summarise(across(
+    .cols = where(is.numeric),
+    .fns  = ~ sum(.x, na.rm = TRUE)
+  )) %>%
+  ungroup()
+
+# rank
+city.overdoses$od.diff.tobacco <- abs(city.overdoses$od.tobacco - city.overdoses$od.tobacco[35])
+city.overdoses <- city.overdoses[order(city.overdoses$od.diff.tobacco),]
+city.overdoses$od.rank.tobacco <- seq.int(nrow(city.overdoses)) -1
+
+# COMBINE
+drugs <- city.overdoses %>%
+  left_join(drug.usage.similar)
+
+
 
 
